5c1653e559751fdcadff06d5c2127bbc7d4fb0be
maxg322 <smaxim152@mail.ru>
2025-03-23 13:41:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 // Copyright 2022 UNN-IASR
-#include "fun.h"
+#include <iostream>
 #include "fun.h"
 
 int main() {
@@ -7,7 +7,8 @@ int main() {
     const char* str2 = "Hello World test A Ab1";
     const char* str3 = "hello world test";
     std::cout << "Words without numbers: " << faStr1(str1) << std::endl;
-    std::cout << "Words with uppercase first letter and lowercase others: " << faStr2(str2) << std::endl;
+    std::cout << "Words with uppercase first "
+    "letter and lowercase others: " << faStr2(str2) << std::endl;
     std::cout << "Average word length: " << faStr3(str3) << std::endl;
     return 0;
 }
